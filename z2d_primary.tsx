46d0265ccfa21e06f4565eadd7e186062099c4c4
mono <bogdan-megamen@yandex.ru>
2023-09-12 14:00:18 +0300

    Initial demo release

---
+++ z2d_primary.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="z2d_primary" tilewidth="128" tileheight="128" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="app/src/main/assets/sprites/tile_grass.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="app/src/main/assets/sprites/tile_r0.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="app/src/main/assets/sprites/tile_r1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="app/src/main/assets/sprites/tile_r2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="app/src/main/assets/sprites/tile_r3.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="app/src/main/assets/sprites/palm0.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="app/src/main/assets/sprites/tree7.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="app/src/main/assets/sprites/foliage2_0.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="app/src/main/assets/sprites/foliage6.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="app/src/main/assets/sprites/foliage7.png"/>
+ </tile>
+</tileset>
